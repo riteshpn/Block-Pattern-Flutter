39c36a95196fdc937ec80954776ee8fac4fbb818
ritesh prasad nandan <prasadritesh261@gmail.com>
2023-03-02 23:15:02 +0530

    This is a simple concept Bloc Pattern

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import 'HomePage.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My Employee App',
+      theme: ThemeData(
+        primarySwatch: Colors.orange
+      ),
+      debugShowCheckedModeBanner: false,
+      home: homePage(),
+
+    );
+  }
+}
